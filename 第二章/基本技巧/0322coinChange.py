1b3ab5a98bceb9c5c26bb48fd6625a3f6c09a48d
yiluohan1234 <1097189275@qq.com>
2021-06-10 10:18:09 +0800

    update fib and coinChange

--- 第二章/基本技巧/0322coinChange.py
+++ 第二章/基本技巧/0322coinChange.py
@@ -12,9 +12,24 @@
 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
 
 你可以认为每种硬币的数量是无限的。
-dp(n)=          0, n=0
-       -1, n <0
-       min(dp(n-coin)+1 |coin in coins, n>0
+dp(n) =  0, n=0
+        -1, n <0
+        min(dp(n-coin)+1 |coin in coins, n>0
+如何列出状态转移方程？
+1、明确状态。原问题和子问题中变化的变量。硬币数量无限，唯一的状态就是目标金额amount。
+2、定义dp数组或函数的定义。dp(n)：当前目标金额为n，至少需要dp(n)个金币凑出目标金额。
+3、明确「选择」并择优。从coins中选择一个硬币，目标金额减少
+# 伪码框架
+def coinChange(coins: List[int], amount: int):
+    # 定义：要凑出金额 n，至少要 dp(n) 个硬币
+    def dp(n):
+        # 做选择，需要硬币最少的那个结果就是答案
+        for coin in coins:
+            res = min(res, 1 + dp(n - coin))
+        return res
+    # 我们要求目标金额是 amount
+    return dp(amount)
+4、明确 base case。目标金额为 0 时，所需硬币数量为 0；当目标金额小于 0 时，无解，返回 -1
 '''
 import unittest
 class Solution(object):
