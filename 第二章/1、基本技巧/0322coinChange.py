a54d65bc684949467a129222bb1315389b70c6da
yiluohan1234 <1097189275@qq.com>
2021-07-12 14:05:22 +0800

    add intervals

--- 第二章/1、基本技巧/0322coinChange.py
+++ 第二章/1、基本技巧/0322coinChange.py
@@ -78,10 +78,10 @@ class Solution(object):
         dp = [amount + 1 for i in range(amount+1)]
         dp[0] = 0
         for i in range(len(dp)):
-            for coin in coins:
-                if i - coin < 0:
+           for j in range(0, len(coins)):
+                if i - coins[j] < 0:
                     continue
-                dp[i] = min(dp[i], 1+dp[i-coin])
+                dp[i] = min(dp[i], dp[i-coins[j]] + 1)
         
         return -1 if (dp[amount] == amount + 1) else dp[amount]
             
